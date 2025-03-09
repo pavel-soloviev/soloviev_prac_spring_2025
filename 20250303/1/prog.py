0c2bd89037e101d3ff26f4edc1e84ab00c00b986
Pavel <sol.pablo2015@yandex.ru>
2025-03-09 23:15:56 +0300

    New monster jgsbat

--- 20250303/1/prog.py
+++ 20250303/1/prog.py
@@ -1,5 +1,23 @@
 import sys
+import shlex
 import cowsay
+from io import StringIO
+
+jgsbat = cowsay.read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs     __\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
 
 GRID_SIZE = 10
 
@@ -35,39 +53,57 @@ def move(direction):
         encounter(x, y)
 
 
-def add_monster(x, y, name, hello):
+def add_monster(name, hp, x, y, hello):
     try:
-        x, y = int(x), int(y)
-        if x < 0 or x > 9 or y < 0 or y > 9:
+        x, y, hp = int(x), int(y), int(hp)
+        if x < 0 or x > 9 or y < 0 or y > 9 or hp <= 0:
             raise ValueError
     except ValueError:
         print("Invalid arguments")
         return
 
     available_cows = cowsay.list_cows()
-    if name not in available_cows:
+    if name not in available_cows and name != "jgsbat":
         print("Cannot add unknown monster")
     else:
         replaced = (x, y) in monsters
-        monsters[(x, y)] = (name, hello)
-        print(f"Added monster {name} to ({x}, {y}) saying {hello}")
+        monsters[(x, y)] = (name, hello, hp)
+        print(
+            f"Added monster {name} to ({x}, {y}) saying {hello}")
         if replaced:
             print("Replaced the old monster")
 
 
 def encounter(x, y):
-    print(cowsay.cowsay(monsters[(x, y)][1], cow=monsters[(x, y)][0]))
+    name, hello, hp = monsters[(x, y)]
+    if name == "jgsbat":
+        print(cowsay.cowsay(f"{hello}", cowfile=jgsbat))
+    else:
+        print(cowsay.cowsay(f"{hello}", cow=name))
 
 
 def parse_commands(command):
-    parts = command.strip().split()
+    parts = shlex.split(command)
     if not parts:
         return
 
     if parts[0] in ("up", "down", "left", "right"):
         move(parts[0])
-    elif parts[0] == "addmon" and len(parts) == 5:
-        add_monster(parts[2], parts[3], parts[1], parts[4])
+    elif parts[0] == "addmon":
+        try:
+            params = {}
+            i = 2
+            while i < len(parts):
+                if parts[i] == "coords":
+                    params["coords"] = (parts[i + 1], parts[i + 2])
+                    i += 3
+                else:
+                    params[parts[i]] = parts[i + 1]
+                    i += 2
+            add_monster(parts[1], params["hp"], params["coords"]
+                        [0], params["coords"][1], params["hello"])
+        except (IndexError, KeyError, ValueError):
+            print("Invalid addmon command format")
     else:
         print("Invalid command")
 
