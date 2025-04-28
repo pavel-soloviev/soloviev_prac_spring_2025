0b96e13d1eabda3d1c03a9a41a2284d3a2c28b99
Pavel <sol.pablo2015@yandex.ru>
2025-04-28 14:22:16 +0300

    cw

--- 20250428/0/old/dodo.py
+++ 20250428/0/old/dodo.py
@@ -1,12 +1,16 @@
 from pathlib import Path
+import zipfile
 
 DOIT_CONFIG = {'default_tasks': ['docs']}
 
 def task_docs():
     """Build documentation"""
-    return {
-            "file_dep": [*Path(".").glob("*.py"), *Path(".").glob("*.rst")]
-            "actions": ['sphinx-build -M html ./source/ _build']
+    deps = [*Path(".").glob("*.py"), *Path(".").glob("*.rst")]
+    for kind in ("html", "text"):
+        yield {
+               "name": kind,
+               "file_dep": deps,
+               "actions": [f"sphinx-build -M {kind} ./source/ _build"],
     }
 
 def task_erasea():
@@ -14,3 +18,30 @@ def task_erasea():
     return {
             "actions": ["git reset --hard", "git clean -xdf"]
     }
+
+def task_zip():
+    """Make zip archive from documentation"""
+    return {
+            "actions": ["zip -r docs.zip _build/html"],
+            "task_dep": ["docs"],
+    }
+
+def task_stat():
+    """Stat"""
+    return {
+            "actions": [f"python3 -m zipfile -l docs.zip > docs.list"],
+            "file_dep": ["docs.zip"],
+            "targets": ["docs.list"],
+    }
+
+def list_zip_contents(zip_path, output_file):
+    with zipfile.ZipFile(zip_path, 'r') as zf:
+        for file_info in zf.infolist():
+            print(f"{file_info.filename.ljust(40)} {file_info.file_size:>10}", file=output_file)
+
+def task_stat1():
+    return {
+            "actions": [(list_zip_contents, ["_build/html", "docs1.list"])],
+            "targets": ["docs1.list"],
+    }
+
