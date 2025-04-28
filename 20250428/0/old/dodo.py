2ce5baa3d40a2fb7acdb92881be2c52cf05b1c13
Pavel <sol.pablo2015@yandex.ru>
2025-04-28 13:33:52 +0300

    nice

--- 20250428/0/old/dodo.py
+++ 20250428/0/old/dodo.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
+DOIT_CONFIG = {'default_tasks': ['docs']}
+
 def task_docs():
     """Build documentation"""
     return {
+            "file_dep": [*Path(".").glob("*.py"), *Path(".").glob("*.rst")]
             "actions": ['sphinx-build -M html ./source/ _build']
     }
 
